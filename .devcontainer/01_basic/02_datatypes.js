1a3af3e4d99628e1c4dc3e5fb4345d867869eb8d
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2023-09-17 06:24:20 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3 + 3)
+
+let name = "Pradeep"
+let age = 18
+let isLoggedIn = False
+
+//number => 2to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Pradeep");
+console.log(typeof "undefined");//undefined
+console.log(typeof "null");//object
