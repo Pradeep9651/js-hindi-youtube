c2d131f132f0a5066590f5c64561587b3419480a
Pradeep Yadav <104863759+Pradeep9651@users.noreply.github.com>
2023-10-11 18:32:28 +0000

    datatype_summary

---
+++ .devcontainer/01_basic/datatypes_summary.js
@@ -0,0 +1,19 @@
+//stack(Primitive), Heap(non-Primitive)
+let myYoutubename="Pradeepyadavjarvis"
+let anothername = "myYoutubename"
+anothername ="chaiAurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne  = {
+    email:"user@google".localeCompare,
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "pradeep@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
